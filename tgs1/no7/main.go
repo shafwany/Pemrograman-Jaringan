3aef133bcf5023517e45e45d123d68beb60ced20
Ahmad Shafwany <42903984+shafwany@users.noreply.github.com>
2019-01-30 18:27:43 +0700

    Update main.go

--- tgs1/no7/main.go
+++ tgs1/no7/main.go
@@ -1,56 +1,40 @@
 package main
 
 import (
-	"fmt"
-	"log"
 	"net/http"
-
-	"github.com/shafwany/Pemrograman-Jaringan/tree/master/tgs1/no7/config"
-	"github.com/spf13/viper"
+	"log"
+	"flag"
+	"fmt"
 )
 
-/*
 var (
 	cnf        *Config
 	configPath string
 )
+
 func initConfig() error {
 	flag.StringVar(&configPath, "c", "config.yaml", "Configuration File")
 	flag.Parse()
+
 	c, err := NewCfg(configPath)
 	if err != nil {
 		return err
 	}
 	cnf = c
+
 	return err
 }
-*/
+
 func main() {
-	viper.SetConfigName("config")
-	viper.AddConfigPath(".")
-	var configuration config.Configuration
-	/*
-		err := initConfig()
-		if err != nil {
-			log.Fatal(err)
-		}
-	*/
-	if err := viper.ReadInConfig(); err != nil {
-		log.Fatalf("Error reading config file, %s", err)
-	}
-	err := viper.Unmarshal(&configuration)
-	if err != nil {
-		log.Fatalf("Unable to decode into struct, %v", err)
-	}
+  err := initConfig()
+  if err != nil {
+	log.Fatal(err)
+  }
+	
+  fs := http.FileServer(http.Dir(cnf.HttpCfg().Dir))
+  http.Handle("/", fs)
 
-	log.Printf("Database = %s", configuration.Database.ConnectionUri)
-	log.Printf("Port = %d", configuration.Server.Port)
-	log.Printf("Directory = %s", configuration.Directory.Dir)
-
-	fs := http.FileServer(http.Dir(configuration.Directory.Dir))
-	http.Handle("/", fs)
-
-	log.Println("Listening...")
-	port := fmt.Sprintf(":%d", configuration.Server.Port)
-	http.ListenAndServe(port, nil)
+  log.Println("Listening...")
+  port := fmt.Sprintf(":%d", cnf.HttpCfg().Port)
+  http.ListenAndServe(port, nil)
 }
